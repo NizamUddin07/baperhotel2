b18079f057276691dc457c131428154ae240ea12
MD NIZAM UDDIN <53169290+NizamUddin07@users.noreply.github.com>
2024-09-08 23:59:39 +0600

    deposite

--- js/deposite.js
+++ js/deposite.js
@@ -2,5 +2,10 @@ document.getElementById('btn-deposite').addEventListener('click', function(){
     const depositeField = document.getElementById('deposite-field');
     const depositeAmountString = depositeField.value;
     const depositeAmount = parseFloat(depositeAmountString);
-    console.log(depositeAmount)
+    
+
+    const depositeTotalElement = document.getElementById('deposite-total');
+    const previousDepositeAmountString = depositeTotalElement.innerText;
+    const previousDepositeAmount = parseFloat(depositeTotalElement);
+
 })
